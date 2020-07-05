8ee8094bd456e87bfa325bd4e5230a8b22f5edaa
kbjude <kbjude@gmail.com>
2020-07-05 07:44:59 +0300

    WIP Setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :models
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :welcome
+  resources :users
   root 'welcome#index'
 end
