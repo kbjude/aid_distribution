faaa2faff4ed016fccb5fa5bab41510582b8fd19
kbjude <kbjude@gmail.com>
2020-07-30 21:31:43 +0300

    Referencing a user comment added

--- db/migrate/20200730181511_create_register_camps.rb
+++ db/migrate/20200730181511_create_register_camps.rb
@@ -1,5 +1,6 @@
 class CreateRegisterCamps < ActiveRecord::Migration[5.2]
   def change
+    #This is still breaking, need to fix the referencing a devise user
     create_table :register_camps do |t|
       t.string :name, null: false, default: ' '
       t.string :location, null: false, default: ' '
