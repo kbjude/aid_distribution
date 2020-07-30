216a6ff9a098ea73a7a6eb4b9fe27f7b1be4aff8
kbjude <kbjude@gmail.com>
2020-07-30 21:24:19 +0300

    Fixes

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,9 @@
 class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 end
--- db/migrate/20200730181511_create_register_camps.rb
+++ db/migrate/20200730181511_create_register_camps.rb
@@ -1,8 +1,8 @@
 class CreateRegisterCamps < ActiveRecord::Migration[5.2]
   def change
     create_table :register_camps do |t|
-      t.string :name,       null: false, default: " "
-      t.string :location,   null: false, default: " "
+      t.string :name, null: false, default: ' '
+      t.string :location, null: false, default: ' '
       t.text :description
       t.string :contacts
       t.users :references
